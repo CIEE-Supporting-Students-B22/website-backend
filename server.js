fda7e54a4a140f0b3b9acdaa7303440251a5237e
NicholasMarkou <nmarkou2000@gmail.com>
2022-11-09 18:11:02 +0100

    Added database integration

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express'),
     app = express(),
+    env = require('dotenv').config(),
+    { MongoClient, ServerApiVersion, ObjectId} = require('mongodb'),
     port = 2000;
+
+const uri = `mongodb+srv://CIEESupportingStudents:${process.env.MONGO_PW}@support.emb45jc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect();
+const db = client.db("CIEESupport")
 
 app.use(express.json());
 
-app.get('/test', (req,res) => {
-    res.send('Test get request')
-})
-
 app.get('/pageTypes', (req, res) => {
     //Essentially the 4 buttons on the homepage
-    let pageTypes = [
-        {postType : "things-to-do", title : "Things To Do"},
-        {postType : "language-prep", title : "Language Prep"},
-        {postType : "using-services", title : "Using Services in the Czech Republic"},
-        {postType : "upcoming-trips", title : "Upcoming Trips offered by CIEE"}
-    ]
-    res.send(pageTypes);
+    let pages = db.collection("Pages").find({}).toArray().then( arr => res.send(arr))
+})
+
+app.post('/getPosts', (req,res) => {
+    db.collection("Posts").find({"postType": req.body.postType}).toArray().then(arr => res.send(arr));
 })
 
 app.post('/removePost', (req,res) => {
-    console.log(req.body);
+    db.collection("Posts").deleteOne({"_id": ObjectId(req.body._id)}).then(res.sendStatus(200))
 })
 
 
