1d1b0ed652cf66056953152da1e552f5329339b7
NicholasMarkou <nmarkou2000@gmail.com>
2022-11-09 15:22:39 +0100

    Test page type request, used to generate admin page on react

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express'),
     app = express(),
-    port = 3000;
+    port = 2000;
 
-app.get('/', (req, res) => {
-    res.send("Test");
+app.use(express.json());
+
+app.get('/test', (req,res) => {
+    res.send('Test get request')
 })
 
+app.get('/pageTypes', (req, res) => {
+    //Essentially the 4 buttons on the homepage
+    let pageTypes = [
+        {postType : "things-to-do", title : "Things To Do"},
+        {postType : "language-prep", title : "Language Prep"},
+        {postType : "using-services", title : "Using Services in the Czech Republic"},
+        {postType : "upcoming-trips", title : "Upcoming Trips offered by CIEE"}
+    ]
+    res.send(pageTypes);
+})
+
+app.post('/removePost', (req,res) => {
+    console.log(req.body);
+})
 
 
 app.listen(port, () => {
